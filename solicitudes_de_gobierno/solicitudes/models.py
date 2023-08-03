a5c9a4d3dc3ac94e9303f98b5bb37c97e779f93b
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-03 16:32:38 -0600

    created usuarios and tiposdeusuario

--- solicitudes_de_gobierno/solicitudes/models.py
+++ solicitudes_de_gobierno/solicitudes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+class TiposDeUsuario(models.Model):
+    nombre = models.CharField(max_length=20)
+    activo = models.BooleanField()
+    fecha_de_registro = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "tipos de usuario"
+
 # Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=20)
+    segundo_nombre = models.CharField()
+    apellido = models.CharField()
+    segundo_apellido = models.CharField()
+    fecha_de_nacimiento = models.DateField()
+    tipo_de_usuario = models.ForeignKey(
+        TiposDeUsuario,
+        on_delete=models.CASCADE
+    )
+    # login_key = models.CharField()
+    activo = models.BooleanField()
+    fecha_de_registro = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "usuarios"
+
+
