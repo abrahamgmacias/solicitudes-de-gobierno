1c68e27b260c099ee2b3b13049bb40825bf8b590
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-03 16:32:52 -0600

    modified tiposdeusuario and usuarios

--- solicitudes_de_gobierno/solicitudes/models.py
+++ solicitudes_de_gobierno/solicitudes/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class TiposDeUsuario(models.Model):
-    nombre = models.CharField(max_length=20)
-    activo = models.BooleanField()
-    fecha_de_registro = models.DateField()
+    nombre = models.CharField(max_length=20, null=False)
+    activo = models.BooleanField(null=False)
+    fecha_de_registro = models.DateField(null=False)
 
     def __str__(self):
         return self.nombre
@@ -13,18 +13,19 @@ class TiposDeUsuario(models.Model):
 
 # Create your models here.
 class Usuarios(models.Model):
-    nombre = models.CharField(max_length=20)
-    segundo_nombre = models.CharField()
-    apellido = models.CharField()
-    segundo_apellido = models.CharField()
-    fecha_de_nacimiento = models.DateField()
+    nombre = models.CharField(max_length=50, null=False)
+    segundo_nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50, null=False)
+    segundo_apellido = models.CharField(max_length=50)
+    fecha_de_nacimiento = models.DateField(null=False)
     tipo_de_usuario = models.ForeignKey(
         TiposDeUsuario,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=False
     )
     # login_key = models.CharField()
-    activo = models.BooleanField()
-    fecha_de_registro = models.DateField()
+    activo = models.BooleanField(null=False)
+    fecha_de_registro = models.DateField(null=False)
 
     def __str__(self):
         return self.nombre
