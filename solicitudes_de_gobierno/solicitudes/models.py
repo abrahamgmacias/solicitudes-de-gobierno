cf8dd01829f46cad3ad968058b2a5a5c2242c12b
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-03 20:42:26 -0600

    adds six new models

--- solicitudes_de_gobierno/solicitudes/models.py
+++ solicitudes_de_gobierno/solicitudes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class TiposDeUsuario(models.Model):
     nombre = models.CharField(max_length=20, null=False)
     activo = models.BooleanField(null=False)
-    fecha_de_registro = models.DateField(null=False)
+    fecha_de_creacion = models.DateField(null=False)
 
     def __str__(self):
         return self.nombre
@@ -11,7 +11,6 @@ class TiposDeUsuario(models.Model):
     class Meta:
         verbose_name = "tipos de usuario"
 
-# Create your models here.
 class Usuario(models.Model):
     nombre = models.CharField(max_length=50, null=False)
     segundo_nombre = models.CharField(max_length=50)
@@ -25,7 +24,7 @@ class Usuario(models.Model):
     )
     # login_key = models.CharField()
     activo = models.BooleanField(null=False)
-    fecha_de_registro = models.DateField(null=False)
+    fecha_de_creacion = models.DateField(null=False)
 
     def __str__(self):
         return self.nombre
@@ -34,3 +33,80 @@ class Usuario(models.Model):
         verbose_name = "usuarios"
 
 
+class TipoDeSolicitud(models.Model):
+    nombre = models.CharField(max_length=100, null=False)
+    descripcion = models.CharField(max_length=500, null=False)
+    activo = models.BooleanField(null=False)
+    fecha_de_creacion = models.DateField(null=False)
+
+    class Meta:
+        verbose_name = "tipo_de_solicitud"
+        verbose_name_plural = "tipos_de_solicitud"
+
+class Solicitud(models.Model):
+    tipo_solicitud_id = models.ForeignKey(
+        TipoDeSolicitud, 
+        null=True
+    )
+    usuario_id = models.ForeignKey(
+        Usuario,
+        null=False
+    )
+    activo = models.BooleanField(null=False)
+    fecha_de_creacion = models.DateField(null=False)
+
+    class Meta:
+        verbose_name_plural = "solicitudes"
+
+class Comentario(models.Model):
+    solicitud_id = models.ForeignKey(
+        Solicitud,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    texto = models.CharField(max_length=500)
+    activo = models.BooleanField(null=False)
+    usuario_id = models.ForeignKey(
+        Usuario,
+        null=False
+    )
+    fecha_de_creacion= models.DateField(null=False),
+
+class Reporte(models.Model):
+    solicitud_id = models.ForeignKey(
+        Solicitud,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    activo = models.BooleanField(null=False)
+    usuario_id = models.ForeignKey(
+        Usuario,
+        null=False
+    )
+    fecha_de_creacion= models.DateField(null=False)
+
+class Estatus(models.Model):
+    nombre = models.IntegerField(null=False)
+    descripcion = models.CharField(max_length=500, null=False),
+    activo = models.BooleanField(null=False)
+    fecha_de_registro = models.DateField(null=False)
+
+    class Meta:
+        verbose_name_plural = "Estatuses"
+
+class HistorialDeSolicitud(models.Model):
+    solicitud_id = models.ForeignKey(
+        Solicitud,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    estatus_id = models.ForeignKey(
+        Estatus,
+        null=False
+    )
+    activo = models.BooleanField(null=False)
+    fecha_de_creacion = models.DateField(null=False)
+
+    class Meta:
+        verbose_name = "historial_de_solicitud"
+        verbose_name_plural = "historial_de_solicitudes"
