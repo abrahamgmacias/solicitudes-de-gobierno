35cb723da5b1bbe62a40a633ef8df3a31ac0c536
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-04 22:49:56 -0600

    removes auth urls

--- solicitudes_de_gobierno/solicitudes_de_gobierno/urls.py
+++ solicitudes_de_gobierno/solicitudes_de_gobierno/urls.py
@@ -20,6 +20,5 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.Index, name="landing-page"),
-    path("", include('auth.urls'))
+    path("", views.Index, name="landing-page")
 ]
