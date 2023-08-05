7e8dfc9d262a329d8d2722e0763c0c775614308e
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-05 12:04:09 -0600

    adds usuarios/

--- solicitudes_de_gobierno/solicitudes_de_gobierno/urls.py
+++ solicitudes_de_gobierno/solicitudes_de_gobierno/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path("", views.Index, name="landing-page"),
+    path("usuarios/", include("usuarios.urls")),
     path("admin/", admin.site.urls),
-    path("", views.Index, name="landing-page")
 ]
