ece92a5e1d0d13792b54416a0a49d1b246386536
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-04 22:55:44 -0600

    adds models

--- solicitudes_de_gobierno/solicitudes/admin.py
+++ solicitudes_de_gobierno/solicitudes/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import TiposDeUsuario, Usuario
+from .models import TipoDeSolicitud, Solicitud, Comentario, Reporte, Estatus, HistorialDeSolicitud
 
 # Register your models here.
-admin.site.register(TiposDeUsuario)
-admin.site.register(Usuario)
+admin.site.register(TipoDeSolicitud)
+admin.site.register(Solicitud)
+admin.site.register(Comentario)
+admin.site.register(Reporte)
+admin.site.register(Estatus)
+admin.site.register(HistorialDeSolicitud)
