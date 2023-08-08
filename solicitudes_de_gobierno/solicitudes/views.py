f1899a49b5e915a2e9a04543dd91953934d00603
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-07 23:32:57 -0600

    adds functional registrarSolicitud

--- solicitudes_de_gobierno/solicitudes/views.py
+++ solicitudes_de_gobierno/solicitudes/views.py
@@ -1,22 +1,18 @@
 import json
+from usuarios.models import Usuario
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import TipoDeSolicitud, Solicitud, HistorialDeSolicitud, Estatus
+from .models import Accion, Solicitud, HistorialDeSolicitud, Estatus, Espacio, Prioridad
 
-
-
-@csrf_exempt
 def registrarHistorialDeSolicitud(solicitud_id):
     estatus_iniciado = 1
-    estatus_instance = Estatus.objects.get(estatus_id=estatus_iniciado)
+    # estatus_instance = Estatus.objects.get(estatus_id=estatus_iniciado)
 
-    HistorialDeSolicitud.objects.create(
-        estatus_id=estatus_instance,
+    return HistorialDeSolicitud.objects.create(
+        estatus_id=estatus_iniciado,
         solicitud_id=solicitud_id,
     )
-
-    return 
 
 
 @csrf_exempt
@@ -30,26 +26,33 @@ def actualizarHistorialDeSolicitud(request):
 def registrarSolicitud(request):
     if request.method == 'POST': 
         data = json.loads(request.body.decode())
-        
-        accion_id = Estatus.objects.get(id=data['accion_id'])
-        espacio_id = Estatus.objects.get(id=data['espacio_id'])
-        usuario_id = Estatus.objects.get(id=data['usuario_id'])
-        prioridad_id = Estatus.objects.get(id=data['prioridad_id'])
 
-        solicitud = Solicitud.objects.create(
-            accion_id=accion_id,
-            espacio_id=espacio_id,
-            usuario_id=usuario_id,
-            informacion_adicional=data['informacion_adicional'],
-            direccion=data['direccion'],
-            estado=data['estado'],
-            municipio_ciudad=['municipio_ciudad'],
-            codigo_postal=['codigo_postal'],
-            prioridad_id=prioridad_id,
-        )
-        solicitud.save()
+        # accion_instance = Accion.objects.get(id=data['accion_id'])
+        # espacio_instance = Espacio.objects.get(id=data['espacio_id'])
+        # usuario_instance = Usuario.objects.get(id=data['usuario_id'])
+        # prioridad_instance = Prioridad.objects.get(id=data['prioridad_id'])
 
-        registrarHistorialDeSolicitud(request, solicitud.id)
+        try:
+            solicitud = Solicitud.objects.create(
+                accion_id=data['accion_id'],
+                espacio_id=data['espacio_id'],
+                usuario_id=data['usuario_id'],
+                informacion_adicional=data['informacion_adicional'],
+                direccion=data['direccion'],
+                estado=data['estado'],
+                municipio_ciudad=data['municipio_ciudad'],
+                codigo_postal=data['codigo_postal'],
+                prioridad_id=data['prioridad_id'],
+            )
+            solicitud.save()
+
+            registrarHistorialDeSolicitud(solicitud.id)
+
+        except:
+            return JsonResponse(status=400, data={"res": "No se pudo registrar la solicitud correctamente."})
+            
+        return JsonResponse(status=200, data={"res": "Se registró la solicitud correctamente."})
+
 
 
 
