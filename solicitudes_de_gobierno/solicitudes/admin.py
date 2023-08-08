91d153c6e881b55b977f77ac2277642e931545f2
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-07 19:22:55 -0600

    adds new models

--- solicitudes_de_gobierno/solicitudes/admin.py
+++ solicitudes_de_gobierno/solicitudes/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import TipoDeSolicitud, Solicitud, Comentario, Reporte, Estatus, HistorialDeSolicitud
+from .models import Prioridad, Accion, Espacio, Solicitud, Comentario, Reporte, Estatus, HistorialDeSolicitud
 
 # Register your models here.
-admin.site.register(TipoDeSolicitud)
 admin.site.register(Solicitud)
 admin.site.register(Comentario)
 admin.site.register(Reporte)
 admin.site.register(Estatus)
 admin.site.register(HistorialDeSolicitud)
+admin.site.register(Accion)
+admin.site.register(Espacio)
+admin.site.register(Prioridad)
