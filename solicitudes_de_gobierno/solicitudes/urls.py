7d828eded4c4e41106a4e406990ee4811c0f80ea
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-08 00:01:22 -0600

    adds /actualizar

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("registrar", views.registrarSolicitud, name="registrar-solicitud")
+    path("registrar", views.registrarSolicitud, name="registrar-solicitud"),
+    path("actualizar/<int:solicitud_id>", views.actualizarEstatusDeSolicitud, name="actualizar-solicitud")
 ]
