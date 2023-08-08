938b81e97dca6558e7995fef35ac71789b029303
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-08 00:14:10 -0600

    adds /historial

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("registrar", views.registrarSolicitud, name="registrar-solicitud"),
-    path("actualizar/<int:solicitud_id>", views.actualizarEstatusDeSolicitud, name="actualizar-solicitud")
+    path("actualizar/<int:solicitud_id>", views.actualizarEstatusDeSolicitud, name="actualizar-solicitud"),
+    path("historial/<int:solicitud_id>", views.getHistorialDeSolicitud, name="historial-de-solicitud")
 ]
