45b756c4590829efa98a51e9b8ef91215320e2c5
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-11 11:02:23 -0600

    adds /solicitud_id

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("registrar", views.registrarSolicitud, name="registrar-solicitud"),
-    path("actualizar/<int:solicitud_id>", views.actualizarSolicitud, name="actualizar-solicitud"),
-    path("historial/<int:solicitud_id>", views.getHistorialDeSolicitud, name="historial-de-solicitud"),
-    path("eliminar/<int:solicitud_id>", views.eliminarSolicitud, name="eliminar-solicitud"),
-    path("<int:solicitud_id>", views.getDataSolicitud, name="get-solicitud"),
+    # path("registrar", views.registrarSolicitud, name="registrar-solicitud"),
+    # path("actualizar/<int:solicitud_id>", views.actualizarSolicitud, name="actualizar-solicitud"),
+    # path("historial/<int:solicitud_id>", views.getHistorialDeSolicitud, name="historial-de-solicitud"),
+    # path("eliminar/<int:solicitud_id>", views.eliminarSolicitud, name="eliminar-solicitud"),
+    # path("<int:solicitud_id>", views.getDataSolicitud, name="get-solicitud"),
 
+    path("<int:solicitud_id>", views.manageSolicitud, name="solicitud"),
     path("<int:solicitud_id>/comentarios", views.manageComentarios, name="comentarios")
 ]
