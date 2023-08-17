43fd1d3a926a5e685d3e4372d01de57c354a2220
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-17 16:24:51 -0600

    changes var names

--- solicitudes_de_gobierno/solicitudes_de_gobierno/views.py
+++ solicitudes_de_gobierno/solicitudes_de_gobierno/views.py
@@ -4,6 +4,6 @@ from usuarios.views import validarExistenciaUsuario, getDataUsuario
 
 def index(request):
     sample_usuario = 1
-    usuario_instance = getDataUsuario(sample_usuario)
+    usuario = getDataUsuario(sample_usuario)
 
-    return render(request, 'index.html', {'mi_ciudad': usuario_instance['data']['municipio_ciudad']})
+    return render(request, 'index.html', {'usuario': usuario['data']})
