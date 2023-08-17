963ebaa443827e09217467677a6f42531eaa8b56
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-16 19:08:43 -0600

    adds /registrar

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path("eliminar/<int:solicitud_id>", views.eliminarSolicitud, name="eliminar-solicitud"),
     # path("<int:solicitud_id>", views.getDataSolicitud, name="get-solicitud"),
     path("mis-solicitudes", views.misSolicitudesView, name="ver-solicitudes"),
+    path("mis-solicitudes/registrar", views.crearSolicitudView, name="crear-solicitudes"),
     path("mis-solicitudes/<int:solicitud_id>", views.solicitudView, name="solicitud-id"),
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
     path("<int:solicitud_id>/comentarios", views.manageComentarios, name="comentarios")
