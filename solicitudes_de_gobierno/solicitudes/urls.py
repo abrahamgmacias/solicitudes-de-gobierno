c2e02e4207d0259c19660b8928daff30b18c4ccd
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-17 12:15:51 -0600

    adds /solicitudes-locales

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("mis-solicitudes", views.misSolicitudesView, name="ver-solicitudes"),
+    path('solicitudes-locales/', views.recientesSolicitudesLocalesView, name='solicitudes-locales'),
+
+    path("mis-solicitudes/", views.misSolicitudesView, name="ver-solicitudes"),
     path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitudes"),
     path("mis-solicitudes/<int:solicitud_id>", views.solicitudView, name="solicitud-id"),
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
+
+
     path("<int:solicitud_id>/comentarios", views.manageComentarios, name="comentarios")
 ]
