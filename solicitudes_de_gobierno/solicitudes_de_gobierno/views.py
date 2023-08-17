09a37b83d69e53ba28d2f40a06a47331b15b2aa2
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-16 23:35:38 -0600

    changes index values

--- solicitudes_de_gobierno/solicitudes_de_gobierno/views.py
+++ solicitudes_de_gobierno/solicitudes_de_gobierno/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from usuarios.views import validarExistenciaUsuario, getDataUsuario
 
 def index(request):
-    return render(request, 'index.html')
+    sample_usuario = 1
+    usuario_instance = getDataUsuario(sample_usuario)
+
+    return render(request, 'index.html', {'mi_ciudad': usuario_instance['data']['municipio_ciudad']})
