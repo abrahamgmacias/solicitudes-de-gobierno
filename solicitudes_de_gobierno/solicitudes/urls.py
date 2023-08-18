6702e1682af5600f6ea9d16b47971a213adf03d1
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-18 17:54:18 -0600

    adds gestionarSolicitudes

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('solicitudes-locales/', views.recientesSolicitudesLocalesView, name='solicitudes-locales'),
     path("mis-solicitudes/", views.misSolicitudesView, name="ver-solicitudes"),
-    path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitudes"),
-    path("mis-solicitudes/<int:solicitud_id>", views.solicitudView, name="solicitud-id"),
+    
+    path("mis-solicitudes/<int:solicitud_id>/", views.gestionarSolicitud, name="gestionar-solicitudes"),
+    
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
     path("<int:solicitud_id>/comentarios/<int:comentario_id>", views.gestionarComentarios, name="gestionar-comentarios"),
-    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios")
+    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios"),
 ]
