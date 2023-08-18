1f7f0ed288fb32253547928f49ef18e11f4c1b8c
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-18 12:22:44 -0600

    adds gestionarComentarios

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitudes"),
     path("mis-solicitudes/<int:solicitud_id>", views.solicitudView, name="solicitud-id"),
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
-    path("<int:solicitud_id>/agregar-comentario", views.agregarComentario, name="agregar-comentario")
+    path("<int:solicitud_id>/comentarios/<int:comentario_id>", views.gestionarComentarios, name="gestionar-comentarios"),
+    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios")
 ]
