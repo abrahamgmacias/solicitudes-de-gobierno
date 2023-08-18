e492b17b6afacca1f296ed5e4af5c19268080e57
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-18 10:20:59 -0600

    adds /agregar-comentario

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     path('solicitudes-locales/', views.recientesSolicitudesLocalesView, name='solicitudes-locales'),
-
     path("mis-solicitudes/", views.misSolicitudesView, name="ver-solicitudes"),
     path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitudes"),
     path("mis-solicitudes/<int:solicitud_id>", views.solicitudView, name="solicitud-id"),
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
-
-
-    path("<int:solicitud_id>/comentarios", views.manageComentarios, name="comentarios")
+    path("<int:solicitud_id>/agregar-comentario", views.agregarComentario, name="agregar-comentario")
 ]
