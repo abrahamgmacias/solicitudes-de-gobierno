1d8eb7ddf97149bcf51223910c8c795f4b02ce04
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-21 14:49:01 -0600

    provides custom authenticate

--- solicitudes_de_gobierno/authentication/apps.py
+++ solicitudes_de_gobierno/authentication/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authentication"
+
+    def ready(self):
+        from .auth_backends import custom_auth
---
+++ solicitudes_de_gobierno/authentication/auth_backends/custom_auth.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.backends import ModelBackend
+
+class CustomAuthenticate(ModelBackend):
+    def authenticate(self, request, correo_electronico=None, contrasena=None, **kwargs):
+        from usuarios.models import Usuario
+        try:
+            usuario = Usuario.objects.get(correo_electronico=correo_electronico)
+
+        except Usuario.DoesNotExist:
+            return None
+
+        if usuario.check_password(contrasena, column='contrasena'):
+            return usuario
+
+        return None
