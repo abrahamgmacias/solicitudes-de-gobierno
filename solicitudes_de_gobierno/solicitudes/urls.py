69bedf2d2382126b990393d53cd9a157d65e48b1
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-20 23:16:28 -0600

    changes url to /explore

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -2,16 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('explorar/', views.recientesSolicitudesLocalesView, name='solicitudes-locales'),
+    path('explorar/', views.explorarSolicitudesView, name='explorar-solicitudes'),
     path('<int:solicitud_id>/', views.gestionarSolicitud, name='explorar-solicitud'),
 
 
     path("mis-solicitudes/", views.misSolicitudesView, name="ver-solicitudes"),
-    
     path("mis-solicitudes/<int:solicitud_id>/", views.gestionarSolicitud, name="gestionar-solicitudes"),
     path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitud"),
-    
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
     path("<int:solicitud_id>/comentarios/<int:comentario_id>", views.gestionarComentarios, name="gestionar-comentarios"),
-    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios"),
+    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios")
 ]
