b145c50cfea6fa9ba724c00faceb6f464098ff66
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-21 17:37:32 -0600

    adds custom checarContrasena

--- solicitudes_de_gobierno/authentication/auth_backends/custom_auth.py
+++ solicitudes_de_gobierno/authentication/auth_backends/custom_auth.py
@@ -4,12 +4,12 @@ class CustomAuthenticate(ModelBackend):
     def authenticate(self, request, correo_electronico=None, contrasena=None, **kwargs):
         from usuarios.models import Usuario
         try:
-            usuario = Usuario.objects.get(correo_electronico=correo_electronico)
+            usuario = Usuario.objects.get(correo_electronico=correo_electronico, activo=True)
 
         except Usuario.DoesNotExist:
             return None
 
-        if usuario.check_password(contrasena, column='contrasena'):
+        if usuario.checarContrasena(contrasena):
             return usuario
 
         return None
