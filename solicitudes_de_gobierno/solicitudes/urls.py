336b554e91e7f6334fc57163b3508d288bb7b6d0
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-22 17:07:37 -0600

    adds cache-localizacion

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -4,12 +4,13 @@ from . import views
 urlpatterns = [
     path('explorar/', views.explorarSolicitudesView, name='explorar-solicitudes'),
     path('<int:solicitud_id>/', views.gestionarSolicitud, name='explorar-solicitud'),
-
-
     path("mis-solicitudes/", views.misSolicitudesView, name="ver-solicitudes"),
     path("mis-solicitudes/<int:solicitud_id>/", views.gestionarSolicitud, name="gestionar-solicitudes"),
     path("mis-solicitudes/registrar", views.registrarSolicitudView, name="registrar-solicitud"),
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
     path("<int:solicitud_id>/comentarios/<int:comentario_id>", views.gestionarComentarios, name="gestionar-comentarios"),
-    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios")
+    path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios"),
+
+
+    path("cache-localizacion", views.cacheLocalizacion, name="cache-localizacion")
 ]
