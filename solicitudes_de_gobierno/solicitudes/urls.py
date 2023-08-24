3193dd5757fab7718d801d4948ee9a7e088c28f7
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-08-24 14:50:43 -0600

    frees space

--- solicitudes_de_gobierno/solicitudes/urls.py
+++ solicitudes_de_gobierno/solicitudes/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     path("mis-solicitudes/<int:solicitud_id>/eliminar", views.eliminarSolicitudView, name="eliminar-solicitud"),
     path("<int:solicitud_id>/comentarios/<int:comentario_id>", views.gestionarComentarios, name="gestionar-comentarios"),
     path("<int:solicitud_id>/comentarios", views.gestionarComentarios, name="gestionar-comentarios"),
-
-
     path("cache-localizacion", views.cacheLocalizacion, name="cache-localizacion")
 ]
